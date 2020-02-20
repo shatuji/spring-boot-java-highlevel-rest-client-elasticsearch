0031e57a237abc63719046606896f980c0228a73
wangyongqiang <pinewang@foxmail.com>
2020-02-20 23:22:41 +0800

    demo elasticsearch

--- src/main/java/com/example/aws/elasticsearch/demo/controller/ProfileController.java
+++ src/main/java/com/example/aws/elasticsearch/demo/controller/ProfileController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController()
+@RestController
 public class ProfileController {
 
     private ProfileService service;
--- src/main/java/com/example/aws/elasticsearch/demo/document/ProfileDocument.java
+++ src/main/java/com/example/aws/elasticsearch/demo/document/ProfileDocument.java
@@ -15,4 +15,51 @@ public class ProfileDocument {
     private Location location;
     private List<String> emails;
 
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public List<Technologies> getTechnologies() {
+    return technologies;
+  }
+
+  public void setTechnologies(List<Technologies> technologies) {
+    this.technologies = technologies;
+  }
+
+  public Location getLocation() {
+    return location;
+  }
+
+  public void setLocation(Location location) {
+    this.location = location;
+  }
+
+  public List<String> getEmails() {
+    return emails;
+  }
+
+  public void setEmails(List<String> emails) {
+    this.emails = emails;
+  }
 }
